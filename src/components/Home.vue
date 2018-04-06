83825e85e97a89e5d3f827c41b7a281f18bdc328
David Dean <vermicious.knid.d@gmail.com>
2018-04-06 00:12:47 -0700

    Add Vuetify. Add toolbar. Update styling.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    <h1>Home</h1>
+    <v-toolbar dark color="primary">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title class="white--text">Crypto Investments</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>apps</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>refresh</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+    </v-toolbar>
   </div>
 </template>
 
@@ -11,6 +27,9 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  created () {
+    console.log('home')
   }
 }
 </script>

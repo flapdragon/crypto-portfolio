1c9fdb12d5918d65c3da94f639b9843079f98d2f
David Dean <vermicious.knid.d@gmail.com>
2018-04-06 13:52:25 -0700

    Add card container. Streamline search field.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,38 +5,54 @@
       <font-awesome-icon :icon="icon" class="toolbar-icon-custom" />
       <v-toolbar-title class="white--text">Crypto Investments</v-toolbar-title>
     </v-toolbar>
-    <v-layout row wrap>
-      <v-flex xs6>
-        Search
-      </v-flex>
-      <v-flex xs6>
-        <v-text-field
-          append-icon="search"
-          label="Search"
-          single-line
-          hide-details
-          v-model="search"
-        ></v-text-field>
-      </v-flex>
-    </v-layout>
-    <v-data-table
-      :headers="headers"
-      :items="items"
-      :search="search"
-      hide-actions
-      class="elevation-1"
-    >
-      <template slot="items" slot-scope="props">
-        <td>{{ props.item.name }}</td>
-        <td class="text-xs-left">{{ props.item.price }}</td>
-        <td class="text-xs-left">{{ props.item.marketCap }}</td>
-        <td class="text-xs-left">{{ props.item.volume24h }}</td>
-        <td class="text-xs-left">{{ props.item.circulatingSupply }}</td>
-        <td class="text-xs-left">{{ props.item.change24h }}</td>
-        <td class="text-xs-left">{{ props.item.invested }}</td>
-        <td class="text-xs-left">{{ props.item.usdValue }}</td>
-      </template>
-    </v-data-table>
+    <v-card>
+      <v-container
+        fluid
+        style="min-height: 0;"
+        grid-list-lg>
+        <v-layout row wrap>
+          <v-flex xs4>
+            <v-text-field
+              append-icon="search"
+              label="Search"
+              single-line
+              hide-details
+              v-model="search"
+            ></v-text-field>
+          </v-flex>
+          <v-flex xs8>
+          </v-flex>
+        </v-layout>
+        <v-layout row wrap>
+          <v-flex xs12>
+            <v-data-table
+              :headers="headers"
+              :items="items"
+              :search="search"
+              hide-actions
+              class="elevation-1"
+            >
+              <template slot="items" slot-scope="props">
+                <td>{{ props.item.name }}</td>
+                <td class="text-xs-left">{{ props.item.price }}</td>
+                <td class="text-xs-left">{{ props.item.marketCap }}</td>
+                <td class="text-xs-left">{{ props.item.volume24h }}</td>
+                <td class="text-xs-left">{{ props.item.circulatingSupply }}</td>
+                <td class="text-xs-left">{{ props.item.change24h }}</td>
+                <td class="text-xs-left">{{ props.item.invested }}</td>
+                <td class="text-xs-left">{{ props.item.usdValue }}</td>
+              </template>
+              <template slot="footer">
+                <td colspan="7"></td>
+                <td>
+                  Total: {{ usdValueSum }}
+                </td>
+              </template>
+            </v-data-table>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card>
   </div>
 </template>
 
@@ -49,6 +65,7 @@ export default {
   data () {
     return {
       search: '',
+      usdValueSum: 0,
       headers: [
         {
           text: 'Name',
@@ -77,16 +94,6 @@ export default {
         {
           name: 'Ethereum',
           price: 368.40,
-          marketCap: 36341531041,
-          volume24h: 1104620000,
-          circulatingSupply: 98646400,
-          change24h: -3.42,
-          invested: 6.5405,
-          usdValue: 2409.52
-        },
-        {
-          name: '',
-          price: null,
           marketCap: 36341531041,
           volume24h: 1104620000,
           circulatingSupply: 98646400,

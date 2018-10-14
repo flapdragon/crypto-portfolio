15772bc075be89f57c62d532c544d85ded091608
David Dean <vermicious.knid.d@gmail.com>
2018-10-14 00:20:02 -0700

    Fix toolbar and drawer layout. Add links to nav. TODO: fix router-link styling.

--- ui/src/components/Header.vue
+++ ui/src/components/Header.vue
@@ -1,23 +1,32 @@
 <template>
   <div>
-    <v-navigation-drawer :clipped="clipped" v-model="drawerOpen" enable-resize-watcher app dark class="blue lighten-3">
+    <v-navigation-drawer :clipped="clipped" v-model="drawerOpen" enable-resize-watcher app dark class="light-blue darken-1">
       <v-list>
-        <v-list-tile
-          v-for="item in items"
-          :key="item.title"
-          @click="navigate(item.title)"
-        >
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
+        <router-link to="/" activeClass="active">
+          <v-list-tile @click.stop="drawerOpen = false">
+            <v-list-tile-action>
+              <v-icon>home</v-icon>
+            </v-list-tile-action>
 
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
+            <v-list-tile-content>
+              <v-list-tile-title>Home</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </router-link>
+        <router-link to="/research" activeClass="active">
+          <v-list-tile @click.stop="drawerOpen = false">
+            <v-list-tile-action>
+              <v-icon>bar_chart</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>Research</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </router-link>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar color="primary" fixed app :clipped-left="clipped" dark>
+    <v-toolbar color="light-blue darken-3" fixed app :clipped-left="clipped" dark>
       <!-- <font-awesome-icon :icon="icon" class="toolbar-icon-custom" @click.stop="drawerOpen = !drawerOpen" /> -->
       <v-toolbar-side-icon @click.stop="drawerOpen = !drawerOpen"></v-toolbar-side-icon>
       <font-awesome-icon :icon="icon" class="toolbar-icon-custom" />
